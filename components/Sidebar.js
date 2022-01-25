058c7d2d039a6235be7cece0ca9f417c6aeb3905
Krishna Kalyana <krishnakalyanax@gmail.com>
2022-01-25 19:26:34 +0530

    Tailwind CSS Added

---
+++ components/Sidebar.js
@@ -0,0 +1,12 @@
+import {HomeIcon} from '@heroicons/react/outline'
+function Sidebar (){
+    return(
+        <div>
+            <button>
+            <HomeIcon className="h-5 w-5"/>
+            </button>
+            
+        </div>
+    )
+}
+export default Sidebar
