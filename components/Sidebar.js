1548fea90a155c95bead4a63a1ac68524092872f
Krishna Kalyana <krishnakalyanax@gmail.com>
2022-01-26 13:22:09 +0530

    Next Auth installed

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,6 +2,7 @@ import {HeartIcon, HomeIcon, LibraryIcon, PlusCircleIcon, RssIcon, SearchIcon} f
 function Sidebar (){
     return(
         <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+           <div className="space-y-4">
             <button className="flex items-center space-x-2 hover:text-white">
             <HomeIcon className="h-5 w-5"/>
             <p>Home</p>
@@ -28,6 +29,17 @@ function Sidebar (){
             <p>Your Episodes</p>
             </button>
            <hr className="border-t-[0.1px] border-gray-900" /> 
+           {/* Playlists */}
+           <p className="cursor-pointer hover:text-white">Playlist Name..</p>
+           <p className="cursor-pointer hover:text-white">Playlist Name..</p>
+           <p className="cursor-pointer hover:text-white">Playlist Name..</p>
+           <p className="cursor-pointer hover:text-white">Playlist Name..</p>
+           <p className="cursor-pointer hover:text-white">Playlist Name..</p>
+           <p className="cursor-pointer hover:text-white">Playlist Name..</p>
+           <p className="cursor-pointer hover:text-white">Playlist Name..</p>
+           <p className="cursor-pointer hover:text-white">Playlist Name..</p>
+           <p className="cursor-pointer hover:text-white">Playlist Name..</p>
+           </div>
         </div>
     )
 }
