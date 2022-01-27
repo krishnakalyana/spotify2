42063f018c363a4712358a2c9b70181dd1b3324d
Krishna Kalyana <krishnakalyanax@gmail.com>
2022-01-27 10:59:48 +0530

    Login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,29 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
+import { LOGIN_URL } from "../../../lib/spotify"
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     SpotifyProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+      authorization:LOGIN_URL,
     }),
     // ...add more providers here
   ],
+  secret:process.env.JWT_SECRET,
+  pages:{
+      signIn:'/login'
+  },
+  callbacks:{
+      async jwt({token,account,user}){
+          if(account && user){
+              return {
+                  ...token,
+                  accessToken: account.access_token,
+          }
+          }
+      }
+  }
 })
